71e9c1ad4f00461c36c3afa07c77399a69550f53
5ivek <ivan.kralj@fer.hr>
2018-07-30 17:11:41 +0200

    Initial project add

---
+++ src/main/java/com/web/springpetclinic/SpringpetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.web.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetclinicApplication.class, args);
+    }
+}
